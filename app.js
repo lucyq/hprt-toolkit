413bf77f08b0d0434d01e8a1669d846d99d50e77
lucyq <lucyzqin@gmail.com>
2015-01-01 20:40:04 -0500

    creating a user

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-// npm init --> package.json
-// var fs = require('fs');
-// var https = require('https');
+// APP.JS --- establishes backend
+// LUCY QIN
 
+/////////////////////////////////
+//   D E P E N D E N C I E S   //
+/////////////////////////////////
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -11,7 +13,19 @@ var passportLocal = require('passport-local');
 var passportHttp = require('passport-http');
 var cookieParser = require('cookie-parser');
 var expressSession = require('express-session'); // creates in memory store
+var mongo = require('mongodb');
+var crypto = require('crypto');
+var sendgrid = require('sendgrid')(
+  process.env.SENDGRID_USERNAME,
+  process.env.SENDGRID_PASSWORD
+);
+// var fs = require('fs');
+// var https = require('https');
 
+
+///////////////////////////////////
+//   C O N F I G U R A T I O N   //
+///////////////////////////////////
 var app = express();
 
 // SECURE
@@ -20,29 +34,63 @@ var app = express();
 // 	key: fs.readFileSync(__dirname + '/my.key')
 // }, app);
 
-
+// VIEWS SETUP
 app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 
-
-
-
-// CONFIG
+// OTHER SETUP
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());﻿
 app.use(cookieParser());
 app.use(expressSession({ secret: process.env.SESSION_SECRET || "butterflies",
 						 resave: false, saveUninitialized: false})); // butterflies can be anything
+
+// DATABASE SETUP
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+var ObjectId = require('mongodb').ObjectID;
+
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+
+// PASSPORT SETUP
 app.use(passport.initialize());
 app.use(passport.session());
 
 function verifyCredentials(username, password, done) {
-	// Pretend this is using a real database
-	if (username == password) {
-		done(null, {id: username, name: username}); // user object with other info		
-	} else {
-		done(null,null); // no error but didn't authenticate correctly, validation failed
-	}
+	mongo.Db.connect(mongoUri, function(err, db) {
+		db.collection('users', function(err, collection) {
+			if (username == null || password == null || username == "" || password == "") {
+				done(err,null);
+			} else {
+				collection.find({'username':username}).toArray(function(err, items){
+					if (items.length == 0) {
+						done(null,null);
+					} else {
+						if (items[0].password == crypto.createHash('md5').update(password).digest("hex")) {
+							done(null, JSON.stringify(items[0]));
+						} else {
+							done(null,null);
+						}
+					}
+				});
+			}
+		});
+	});
+	// // Pretend this is using a real database
+	// if (username == password) {
+	// 	done(null, {id: username, name: username}); // user object with other info		
+	// } else {
+	// 	done(null,null); // no error but didn't authenticate correctly, validation failed
+	// }
 }
 
 passport.use(new passportLocal.Strategy(verifyCredentials));
@@ -76,7 +124,10 @@ passport.deserializeUser(function(id, done) {
 });
 
 
-// RESOURCES
+
+///////////////////////////
+//   R E S O U R C E S   //
+///////////////////////////
 //app.set('views', path.join(__dirname, 'views')) // __dirname : folder that contains this file
 // serve contents of this path
 app.use(express.static(path.join(__dirname, 'bower_components'))); 
@@ -92,10 +143,12 @@ function ensureAuthorized(req, res, next) {
 	}
 }
 
+
+/////////////////////
+//   R O U T E S   //
+/////////////////////
 app.use('/api', passport.authenticate('basic', { session: false})); // reauthenticate
 
-
-// Passport Config
 app.get('/api/data', ensureAuthorized, function(req, res) {
 	res.json([
 		{value: 'foo'},
@@ -107,11 +160,78 @@ app.get('/api/data', ensureAuthorized, function(req, res) {
 
 // all the routes added to todos are now added here
 
-app.use(require('./routes'));
-app.use(require('./errors'));
+// app.use(require('./routes'));
+// app.use(require('./errors'));
 
+//
+//   A C C O U N T   M A N A G E M E N T
+//
 app.post('/login', passport.authenticate('local'), function(req,res) {
 	res.redirect('home');
+});
+
+app.get('/create_user', function(req, res) {
+	res.render('create_user', {
+		isAuthenticated: req.isAuthenticated(), 
+		user: req.user
+	});
+})
+
+app.post('/create_user', function(req, res, next){
+	mongo.Db.connect(mongoUri, function (err,db){
+		db.collection('users', function(err, col) {
+			var username = req.body.username;
+			var password = req.body.password;
+			var first_name = req.body.first_name;
+			var last_name = req.body.last_name;
+			var ver_password = req.body.verification_password;
+			var email = req.body.email;
+			var phone_num = req.body.phone_num;
+			var patients = [];
+			var time = new Date();
+
+
+     		// if (username == null || password == null || ver_password == null || email == null || 
+     		// 	first_name == null || last_name == null || phone_num == null) {
+     		// 	res.send('Missing fields!');
+     		// } else if (password != ver_password){
+     		if (password != ver_password) {
+     			res.send("Passwords don't match!");
+     		} else {
+     			col.find({'username':username}).toArray(function(err, items) {
+     				if (items.length != 0) {
+     					res.send("Username has been taken!");
+     				} else {
+     					sendgrid.send({
+     						to: email,
+     						from: "lucyzqin@gmail.com",
+     						subject: "Welcome to HPRT Toolkit!",
+     						text: "Hello " + first_name + " " + last_name + ", welcome to HPRT Toolkit!"	
+     					}, function(err, json) {
+     						if (err) { return console.error(err); }
+     						console.log(json);
+     					});
+     					col.insert({'username':username,
+     								'password':crypto.createHash('md5').update(password).digest("hex"),
+     								'first_name':first_name,
+     								'last_name':last_name,
+     								'email':email,
+     								'phone_num':phone_num,
+     								'patients':patients,
+     								'created_at':time
+
+
+			     					}, {safe: true}, function(err, res) {
+			     						col.find({'username':username}).toArray(function(err, items) {
+			     							res.send(JSON.stringify(items[0]));
+			     						});
+     					});
+     				}
+     			});
+     		}
+		});
+	});
+  
 });
 
 app.get('/login', function(req, res) {
@@ -147,6 +267,56 @@ app.get('/', function (req, res) {
 });
 */
 
+
+// RENDER STATIC PAGES
+app.get('/', function (req, res) {
+	res.render('index', {
+		isAuthenticated: false, 
+		user: req.user
+	});
+});
+
+app.get('/quick_guide', function(req, res){
+	res.render('quick_guide', {
+		isAuthenticated: req.isAuthenticated(),
+		user: req.user		
+	});
+});
+
+app.get('/home', function(req, res){
+	res.render('home', {
+		isAuthenticated: req.isAuthenticated(),
+		user: req.user
+	});
+});
+
+app.get('/manual', function(req, res) {
+	res.render('manual', {
+		isAuthenticated: req.isAuthenticated(),
+		user: req.user
+	});
+});
+
+// Patient Information Pages
+app.get('/add_patient_info', function(req, res) {
+	res.render('add_patient_info', {
+		isAuthenticated: req.isAuthenticated(),
+		user: req.user
+	});
+});
+
+app.get('/view_patient_info', function(req, res) {
+	res.render('view_patient_info', {
+		isAuthenticated: req.isAuthenticated(),
+		user: req.user
+	});
+});
+
+
+
+/////////////////////
+//   S E R V E R   //
+/////////////////////
 var port = process.env.PORT || 5000;
 
 app.listen(port, function() {
--- routes.js
+++ routes.js
@@ -1,83 +1,57 @@
-var express = require('express');
-
-var router = express.Router();
-
-
-var user_bool;
-
-// INDEX 
-router.get('/', function (req, res) {
-	res.render('index', {
-		isAuthenticated: false, 
-		user: req.user
-	});
-});
-
-// MANAGING USER ACCOUNTS
+// var express = require('express');
 
 
 
+// // function verifyCredentials(username, password, done) {
+// // 	// Pretend this is using a real database
+// // 	if (username == password) {
+// // 		done(null, {id: username, name: username}); // user object with other info		
+// // 	} else {
+// // 		done(null,null); // no error but didn't authenticate correctly, validation failed
+// // 	}
+// // }
 
 
-
-// RENDER STATIC ACCOUNTS
-router.get('/quick_guide', function(req, res){
-	res.render('quick_guide', {
-		isAuthenticated: false, 
-		user: req.user		
-	});
-});
-
-router.get('/home', function(req, res){
-	res.render('home', {
-		isAuthenticated: true, 
-		user: req.user
-	});
-});
-
-router.get('/manual', function(req, res) {
-	res.render('manual', {
-		isAuthenticated: true, 
-		user: req.user
-	});
-});
-
-// Patient Information Pages
-router.get('/add_patient_info', function(req, res) {
-	res.render('add_patient_info', {
-		isAuthenticated: true, 
-		user: req.user
-	});
-});
-
-router.get('/view_patient_info', function(req, res) {
-	res.render('view_patient_info', {
-		isAuthenticated: true, 
-		user: req.user
-	});
-});
+// var router = express.Router();
 
 
-router.get('/patients/new', function(req, res) {
-	res.render('patients/new', {
-		isAuthenticated: true, 
-		user: req.user
-	});
-});
+// // var user_bool;
+
+// // // INDEX 
+// // router.get('/', function (req, res) {
+// // 	res.render('index', {
+// // 		isAuthenticated: false, 
+// // 		user: req.user
+// // 	});
+// // });
+
+// // // MANAGING USER ACCOUNTS
 
 
 
 
-// router.post('/add', function (req, res) {
-// 	var newItem = req.body.newItem; 
-// 	todoItems.push({
-// 		id: todoItems.length + 1,
-// 		desc: newItem
 
-// 	});
-// 	res.redirect('/');
-// });
 
-// exports router
-module.exports = router;
+// // router.get('/patients/new', function(req, res) {
+// // 	res.render('patients/new', {
+// // 		isAuthenticated: true, 
+// // 		user: req.user
+// // 	});
+// // });
+
+
+
+
+// // // router.post('/add', function (req, res) {
+// // // 	var newItem = req.body.newItem; 
+// // // 	todoItems.push({
+// // // 		id: todoItems.length + 1,
+// // // 		desc: newItem
+
+// // // 	});
+// // // 	res.redirect('/');
+// // // });
+
+// // exports router
+// module.exports = router;
 
